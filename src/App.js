4750d2cdebfeb53bca97aa2fbfd5fdb1c040dfc3
DavinK1 <takigawaleo@gmail.com>
2024-12-17 16:29:49 +0700

    17 ธ.ค. 2567

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.container}>
-      <p className={styles.headerText}>Index</p>
+      <p className={styles.headerText}>Home</p>
     </div>
   );
 }
